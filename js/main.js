9c6c97a5f7daba1885ddda0c6d26dea5889da4db
Lawrence Liu <lawrence.liu1@accesscfa.com>
2023-01-25 23:34:57 -0600

    refactored three.js init with classes

--- js/main.js
+++ js/main.js
@@ -1,71 +1,62 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
-let camera, scene, renderer;
 
+class World {
+    constuctor () {
 
-init().then(animate)
+    }
 
-async function init() {
-    camera = new THREE.PerspectiveCamera(70, innerWidth / innerHeight, 0.1, 1000)
-    camera.position.z = 15
+    async initialize() {
+        // THREEJS INIT
+        // scene 
+        this.scene = new THREE.Scene()
+        this.scene.background = new THREE.Color(0x6e9b8e)
 
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x6e9b8e)
-
-    renderer = new THREE.WebGLRenderer( {antialias: true })
-    renderer.setPixelRatio( devicePixelRatio )
-    renderer.setSize( innerWidth, innerHeight )
-    document.body.appendChild( renderer.domElement )
-
-    window.addEventListener('resize', () => {
-            renderer.setSize(innerWidth, innerHeight)
-            camera.aspect = innerWidth / innerHeight
-            camera.updateProjectionMatrix()
+        // render
+        this.renderer = new THREE.WebGLRenderer( {
+            antialias: true, 
+            canvas: document.querySelector('canvas')
         })
+        this.renderer.setPixelRatio( devicePixelRatio )
+        this.renderer.setSize( innerWidth, innerHeight )
+        
+        document.body.appendChild( this.renderer.domElement )
+        
+        // camera
+        this.camera = new THREE.PerspectiveCamera(70, innerWidth / innerHeight, 0.1, 1000)
+        this.camera.position.z = 15
 
-    const loader = new THREE.TextureLoader()
-    const texture = await load_image(loader)
-    console.log(texture)
-    texture.anisotropy = renderer.capabilities.getMaxAnisotropy
+        // planes
+        this.loader = new THREE.TextureLoader()
+        this.texture = await this.load_image(this.loader)
+        this.texture.anisotropy = this.renderer.capabilities.getMaxAnisotropy
+        this.geometry = new THREE.PlaneGeometry(16, 9)
+        this.material = new THREE.MeshBasicMaterial({ map: this.texture })
+        this.plane = new THREE.Mesh(this.geometry, this.material)
 
-    const geometry = new THREE.PlaneGeometry(16, 9)
-    const material = new THREE.MeshBasicMaterial({ map: texture })
-    const plane = new THREE.Mesh(geometry, material)
-    
-    scene.add( plane )
+        this.scene.add(this.plane)
 
-    const texture_2 = await load_image_2(loader)
-    console.log(texture)
-    texture.anisotropy = renderer.capabilities.getMaxAnisotropy
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement )
+        this.controls.update()
 
-    const geometry_2 = new THREE.PlaneGeometry(16, 9)
-    const material_2 = new THREE.MeshBasicMaterial({ map: texture_2 })
-    const plane_2 = new THREE.Mesh(geometry_2, material_2)
-    plane_2.position.set(18, 0, 4)
-    plane_2.rotation.y = - Math.PI / 2
+        this.animate()
+        
+    }
 
+    async load_image(loader) {
+        const texture = await loader.loadAsync( '../temp_img.jpg' )
+        return texture
+    }
 
-    scene.add( plane_2 )
-
-    // const ambientLight = new THREE.AmbientLight(0x404040)
-    // scene.add(ambientLight)
-
-    const controls = new OrbitControls( camera, renderer.domElement )
-    controls.update()
-
+    animate() {
+        requestAnimationFrame(this.animate.bind(this))
+        this.renderer.render(this.scene, this.camera)
+    }
 }
 
-async function load_image(loader) {
-    const texture = await loader.loadAsync( '../temp_img.jpg' )
-    return texture
-}
+let APP = null 
 
-async function load_image_2(loader) {
-    const texture = await loader.loadAsync( '../temp_img_2.jpg')
-    return texture
-}
-
-function animate() {
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
-}
+window.addEventListener('DOMContentLoaded', async() => {
+    APP = new World()
+    APP.initialize()
+})
